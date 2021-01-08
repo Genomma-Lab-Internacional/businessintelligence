5eb6270d0cfc449a8c631e0add36529c832f8236
Jesus Hernandez <jesus.hernandez@genommalab.com>
2021-01-08 09:29:06 -0600

    Agregamos un archivo py con las paqueterías

---
+++ Libraries.py
@@ -0,0 +1,18 @@
+#Libraries
+
+import mysql.connector
+import jupyternotify
+import pandas as pd
+import calendar
+import requests
+import pyodbc
+import json
+import os
+from mysql.connector import errorcode
+from mysql.connector import Error
+from urllib.request import urlopen
+from datetime import timedelta
+from datetime import datetime
+from zipfile import ZipFile
+from io import BytesIO
+from time import sleep
