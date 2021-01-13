5964714ef7965cf80093d5ce90bf3393ec2fdeab
Jesus Hernandez <jesus.hernandez@genommalab.com>
2021-01-13 09:08:55 -0600

    Nos quedamos con las librerías necesarias

--- Libraries.py
+++ Libraries.py
@@ -1,16 +1,12 @@
 #Libraries
-
-import jupyternotify
-import pandas as pd
-import calendar
-import requests
-import pyodbc
-import json
 from urllib.request import urlopen
 from datetime import timedelta
 from datetime import datetime
 from zipfile import ZipFile
 from io import BytesIO
 from time import sleep
-import numpy as np
-import matplotlib.pyplot as plt
+import pandas as pd
+import calendar
+import requests
+import pyodbc
+import json
