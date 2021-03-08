63ec7466e07e822a0affb37962ffe42e18ffa202
Jesus Hernandez <jesus.hernandez@genommalab.com>
2021-03-08 14:58:27 -0600

    Se agregó el reproceso de DWH, Colombia y se modicó el método gen_id

--- ISV_Weekly/reproceso.py
+++ ISV_Weekly/reproceso.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Código reproceso para los datos que se almacenan en AWS
 class reproceso_AWS():
     # df_A: DataFrame con los datos anteriores
     # df_B: DataFrame con los datos nuevos. Los datos que se procesarán.
@@ -18,8 +19,8 @@ class reproceso_AWS():
         for col in num_cols:
             A[col] = self._nums_to_string(A, col)
             B[col] = self._nums_to_string(B, col)
-        A['ID'] = A[num_cols].sum(axis=1)
-        B['ID'] = B[num_cols].sum(axis=1)
+        A['ID'] = A['ProPstID'].astype(str) + A['SucID'].astype(str) + A['TmpID'].astype(str)
+        B['ID'] = B['ProPstID'].astype(str) + B['SucID'].astype(str) + B['TmpID'].astype(str)
         return A, B
 
     # Creamos un pivot table para evitar duplicidad en el ID
@@ -86,4 +87,67 @@ class reproceso_AWS():
                     data_filled = pd.concat([data_filled, self._fill_column_so(join, i[0], i[1], i[2])], axis=1)
                 elif k == 'stock':
                     data_filled = pd.concat([data_filled, self._fill_column_stock(join, i[0], i[1], i[2])], axis=1)
+        return data_filled
+
+# Código reproceso para los datos que se almacenan en el DWH
+class reproceso_DWH(reproceso_AWS):
+    def __init__(self, df_A, df_B):
+        super().__init__(df_A, df_B)
+
+    # Función main, donde corre los métodos que se heredan de reproceso_AWS
+    def reprocessing(self):
+        df_A, df_B = self._prepare_data()
+        dcolumns = {'index':[('ProPstID', 'ProPstID_x', 'ProPstID_y'), ('SucID', 'SucID_x', 'SucID_y'), ('TmpID', 'TmpID_x', 'TmpID_y'), ('MonID', 'MonID_x', 'MonID_y')],
+            'sellout':[('SoutCantDesp', 'SoutCantDesp_x', 'SoutCantDesp_y'), ('SoutMontoDesp', 'SoutMontoDesp_x', 'SoutMontoDesp_y'), ('SoutMontoCteDesp', 'SoutMontoCteDesp_x', 'SoutMontoCteDesp_y')],
+            'stock':[('SoutCantCedis', 'SoutCantCedis_x', 'SoutCantCedis_y'), ('SoutCantExist', 'SoutCantExist_x', 'SoutCantExist_y'), ('SoutCantInv', 'SoutCantInv_x', 'SoutCantInv_y'), ('SoutCantTrans', 'SoutCantTrans_x', 'SoutCantTrans_y'), ('SoutMontoExist', 'SoutMontoExist_x', 'SoutMontoExist_y'), ('SoutMontoCedis', 'SoutMontoCedis_x', 'SoutMontoCedis_y'), ('SoutMontoTrans', 'SoutMontoTrans_x', 'SoutMontoTrans_y'), ('SoutMontoInv', 'SoutMontoInv_x', 'SoutMontoInv_y'), ('SoutMontoStaple', 'SoutMontoStaple_x', 'SoutMontoStaple_y'), ('SoutCantStaple', 'SoutCantStaple_x', 'SoutCantStaple_y')]}
+        join = pd.merge(df_A, df_B, on='ID', how='outer')
+        data_filled = pd.DataFrame()
+        for k, v in dcolumns.items():
+            for i in v:
+                if k == 'index':
+                    data_filled = pd.concat([data_filled, self._fill_column_index(join, i[0], i[1], i[2])], axis=1)
+                elif k == 'sellout':
+                    data_filled = pd.concat([data_filled, self._fill_column_so(join, i[0], i[1], i[2])], axis=1)
+                elif k == 'stock':
+                    data_filled = pd.concat([data_filled, self._fill_column_stock(join, i[0], i[1], i[2])], axis=1)
+        return data_filled
+
+# Código reproceso para el visor de Colombia
+class reproceso_COL(reproceso_AWS):
+    def __init__(self, df_A, df_B):
+        super().__init__(df_A, df_B)
+
+    # Generación de ID
+    def _gen_ids(self, A, B, num_cols, str_cols):
+        for col in num_cols:
+            A[col] = self._nums_to_string(A, col)
+            B[col] = self._nums_to_string(B, col)
+        A['ID'] = A['Año_GL'].astype(str) + A['Semana_GL'].astype(str) + A['Canal'] + A['GrpNombre'] + A['ProPstCodBarras'].astype(str)
+        B['ID'] = B['Año_GL'].astype(str) + B['Semana_GL'].astype(str) + B['Canal'] + B['GrpNombre'] + B['ProPstCodBarras'].astype(str)
+        return A, B
+
+    # Creamos un pivot table con los datos y su ID
+    def _prepare_data(self):
+        table_A = self.df_A.pivot_table(index=['Año_GL', 'Semana_GL', 'Canal', 'GrpNombre', 'MrcNombre','AgrProNombre', 'ProPstCodBarras', 'ProPstID', 'ProPstNombre'], values=['Sellout unidades', 'Inventario Unidades', 'Sellout $$$', 'Inventario $$$'], aggfunc='sum').reset_index()
+        table_B = self.df_B.pivot_table(index=['Año_GL', 'Semana_GL', 'Canal', 'GrpNombre', 'MrcNombre','AgrProNombre', 'ProPstCodBarras', 'ProPstID', 'ProPstNombre'], values=['Sellout unidades', 'Inventario Unidades', 'Sellout $$$', 'Inventario $$$'], aggfunc='sum').reset_index()
+        table_A, table_B = self._gen_ids(table_A, table_B, num_cols=['Año_GL', 'Semana_GL', 'ProPstCodBarras'], str_cols=['Canal', 'GrpNombre'])
+        return table_A, table_B
+
+    # Función main, donde corre los métodos que se heredan de reproceso_AWS
+    def reprocessing(self):
+        df_A, df_B = self._prepare_data()
+        dcolumns = {'index':[('Año_GL', 'Año_GL_x', 'Año_GL_y'), ('Semana_GL', 'Semana_GL_x', 'Semana_GL_y'), ('Canal', 'Canal_x', 'Canal_y'), ('GrpNombre', 'GrpNombre_x', 'GrpNombre_y'), ('MrcNombre', 'MrcNombre_x', 'MrcNombre_y'), ('AgrProNombre', 'AgrProNombre_x', 'AgrProNombre_y'), ('ProPstCodBarras', 'ProPstCodBarras_x', 'ProPstCodBarras_y'), ('ProPstID', 'ProPstID_x', 'ProPstID_y'), ('ProPstNombre', 'ProPstNombre_x', 'ProPstNombre_y')],
+            'sellout':[('Sellout unidades', 'Sellout unidades_x', 'Sellout unidades_y'), ('Sellout $$$', 'Sellout $$$_x', 'Sellout $$$_y')],
+            'stock':[('Inventario Unidades', 'Inventario Unidades_x', 'Inventario Unidades_y'), ('Inventario $$$', 'Inventario $$$_x', 'Inventario $$$_y')]}
+        join = pd.merge(df_A, df_B, on='ID', how='outer')
+        data_filled = pd.DataFrame()
+        for k, v in dcolumns.items():
+            for i in v:
+                if k == 'index':
+                    data_filled = pd.concat([data_filled, self._fill_column_index(join, i[0], i[1], i[2])], axis=1)
+                elif k == 'sellout':
+                    data_filled = pd.concat([data_filled, self._fill_column_so(join, i[0], i[1], i[2])], axis=1)
+                elif k == 'stock':
+                    data_filled = pd.concat([data_filled, self._fill_column_stock(join, i[0], i[1], i[2])], axis=1)
+        data_filled = data_filled[['Año_GL', 'Semana_GL', 'Canal', 'GrpNombre', 'MrcNombre','AgrProNombre', 'ProPstCodBarras', 'ProPstID', 'ProPstNombre', ]]
         return data_filled
